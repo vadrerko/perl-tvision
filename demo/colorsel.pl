6e1ba9227860de0ec1c92a9e96859a19168d75b2
U-GPBNB19931\gpbuser <vadrerko@gmail.com>
2025-03-13 20:41:25 +0300

    version 0.18

--- demo/colorsel.pl
+++ demo/colorsel.pl
@@ -1,8 +1,8 @@
 use strict;
 use TVision('tnew');
 
-my $cs1 = tnew(TColorSelector=>[1,1,110,16],0);
-my $cs2 = tnew(TColorSelector=>[1,18,110,36],1);
+my $cs1 = tnew(TColorSelector=>[1,1,15,5],0);
+my $cs2 = tnew(TColorSelector=>[28,1,40,5],1);
 
 my $tapp = tnew('TVApp');
 my $desktop = $tapp->deskTop;
